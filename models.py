2144226abc539611dc5f0e635f951b6a0111f028
belerico <belo.fede@outlook.com>
2020-01-19 15:17:29 +0100

    Redifine models to fit pyGPGO standards

--- models.py
+++ models.py
@@ -12,9 +12,9 @@ from keras.layers import (
     MaxPool2D,
     Dropout,
     BatchNormalization,
-    SimpleRNN
 )
 from keras.initializers import Constant
+from keras.backend.tensorflow_backend import tanh
 
 
 def gen_interaction_matrix(matrix_similarity_function):
@@ -28,6 +28,13 @@ def gen_interaction_matrix(matrix_similarity_function):
     return Lambda(fun)
 
 
+def Tanh():
+    def fun(x):
+        return tanh(x)
+
+    return Lambda(fun)
+
+
 def get_deep_cross_model(
     vocab_size,
     embedding_dimension,
@@ -36,26 +43,30 @@ def get_deep_cross_model(
     convs_depth,
     denses_depth,
     activation,
-    rnn_type='LSTM',
-    rnn_dimension=100,
-    rnn_dropout=0.3,
     embedding_matrix=None,
     embedding_trainable=False,
     embedding_dropout=0.3,
+    rnn_type="LSTM",
+    rnn_units=100,
+    rnn_dropout=0.3,
+    convs_filter_banks=8,
+    convs_kernel_size=2,
+    pool_size=2,
+    denses_units=32,
     mlp_dropout=0.3,
 ):
     left_input = Input((vec_dimension,))
     right_input = Input((vec_dimension,))
 
     if embedding_matrix is None:
-        embed = Embedding(vocab_size, embedding_dimension)
+        embed = Embedding(vocab_size, embedding_dimension, mask_zero=True)
     else:
         embed = Embedding(
             vocab_size,
             embedding_dimension,
             embeddings_initializer=Constant(embedding_matrix),
             trainable=embedding_trainable,
-            mask_zero=True
+            mask_zero=True,
         )
 
     left_encoded = embed(left_input)
@@ -65,27 +76,41 @@ def get_deep_cross_model(
         right_encoded = Dropout(embedding_dropout)(right_encoded)
 
     rnn_dropout = rnn_dropout if rnn_dropout else 0
-    if rnn_type == 'GRU':
-        rnn_left = GRU(rnn_dimension, return_sequences=True, recurrent_dropout=rnn_dropout)
-        rnn_right = GRU(rnn_dimension, return_sequences=True, recurrent_dropout=rnn_dropout)
-    elif rnn_type == 'LSTM':
-        rnn_left = LSTM(rnn_dimension, return_sequences=True, recurrent_dropout=rnn_dropout)
-        rnn_right = LSTM(rnn_dimension, return_sequences=True, recurrent_dropout=rnn_dropout)
+    if rnn_type == "GRU":
+        rnn_left = GRU(
+            rnn_units, return_sequences=True, recurrent_dropout=rnn_dropout
+        )
+        rnn_right = GRU(
+            rnn_units, return_sequences=True, recurrent_dropout=rnn_dropout
+        )
+    elif rnn_type == "LSTM":
+        rnn_left = LSTM(
+            rnn_units, return_sequences=True, recurrent_dropout=rnn_dropout
+        )
+        rnn_right = LSTM(
+            rnn_units, return_sequences=True, recurrent_dropout=rnn_dropout
+        )
 
     bi_left = Bidirectional(rnn_left, merge_mode="concat")(left_encoded)
     bi_right = Bidirectional(rnn_right, merge_mode="concat")(right_encoded)
 
     x = gen_interaction_matrix(matrix_similarity_function)([bi_left, bi_right])
 
-    for conv_depth in convs_depth:
-        x = Conv2D(conv_depth, (2, 2), activation="relu")(x)
-        x = MaxPool2D()(x)
+    for i in range(convs_depth):
+        if convs_filter_banks >= 4:
+            x = Conv2D(convs_filter_banks, convs_kernel_size, activation="relu")(x)
+            x = MaxPool2D(pool_size=pool_size)(x)
+            convs_filter_banks //= 2
 
     x = Flatten()(x)
-    for i, dense_depth in enumerate(denses_depth):
-        x = Dense(dense_depth, activation="relu")(x)
-        if i < len(denses_depth) - 1 and mlp_dropout:
-            x = Dropout(mlp_dropout)(x)
+    x = Tanh()(x)
+
+    for i in range(denses_depth):
+        if denses_units >= 16:
+            x = Dense(denses_units, activation="relu")(x)
+            if i < denses_depth - 1 and mlp_dropout:
+                x = Dropout(mlp_dropout)(x)
+            denses_units //= 2
 
     output = Dense(1, activation=activation)(x)
 
